b4217c2f95b97ca757509f37c10c1d1fdcc94c21
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-09-05 06:39:46 +1000

    Add contract addresses

--- src/contracts/Stats.sol
+++ src/contracts/Stats.sol
@@ -1,9 +1,16 @@
 pragma solidity ^0.4.24;
 
-contract Stats {
-    
-    constructor () public {
+import "./Ownable.sol";
+import "./ERC20.sol";
 
+contract Stats {
+
+    address private waterContract;
+    address private audContract;
+
+    constructor () public {
+        waterContract = 0x0050e270e80c831e42af1979bfccb47d82c797fa52;
+        audContract = 0x00259a416f7cea5ac72c547daf542c67a7d6459283;
     }
 
     function getVolumeAvailable() public pure returns(uint256) {
