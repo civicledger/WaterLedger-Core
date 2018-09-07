04a7970f786afbbe50f173e07b83c967a0460c3d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-09-07 11:05:12 +1000

    Add other fake methods

--- src/contracts/Stats.sol
+++ src/contracts/Stats.sol
@@ -11,6 +11,8 @@ contract Stats is Ownable {
     uint256 private _volumeAvailable;
     uint256 private _todaysSales;
 
+    uint256 public lastUpdated;
+
     constructor () public {
         waterContract = 0x0050e270e80c831e42af1979bfccb47d82c797fa52;
         audContract = 0x00259a416f7cea5ac72c547daf542c67a7d6459283;
@@ -25,5 +27,21 @@ contract Stats is Ownable {
 
     function getTodaysSales() public view returns(uint256) {
         return _todaysSales;
+    }
+
+    function getAverageBid() public pure returns(uint256) {
+        return 0;
+    }
+
+    function getAverageAsk() public pure returns(uint256) {
+        return 0;
+    }
+
+    function getLastBid() public pure returns(uint256) {
+        return 0;
+    }
+
+    function getLastAsk() public pure returns(uint256) {
+        return 0;
     }
 }
